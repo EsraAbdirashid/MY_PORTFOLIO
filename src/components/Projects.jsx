f79b02966fddc0921615030d5010e86fabefc148
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-27 11:10:43 +0300

    Done

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+function Projects() {
+  return (
+    <section className="projects-page">
+      <div className="container">
+        <h1>My Projects</h1>
+        <p>Here are some of my recent projects.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
